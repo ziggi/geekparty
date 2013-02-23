f9c44e26cce4b216ab0deeefe40be3f160b4c6b4
Evgeny Rumiantse <chezrd@gmail.com>
2013-02-23 16:45:53 +0400

    game start/end

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 $(document).ready(function () {
 	var interval_1;
 	var player;
+	var startButton;
 	var timeoutId;
+	var gamestate = 0;
 	var keystate = [false, false, false]; // left, right, mouse
 	var speed = 3;
 	const points_per_frame = 6;
 	var score = 0;
 	const movements = [103.75, 237.5];
-	var up_or_down = true;
-	const keyLeft = 37;
-	const keyRight = 39;
+//	var up_or_down = true;
+//	const keyLeft = 37;
+//	const keyRight = 39;
 	canvas = new fabric.Canvas('canvas');
 
+	prepareResourses();
+
+	$('canvas').click( startGame );
 	$(document).mousedown(function()
 	{
+		//startGame();
 		keystate[2] = true;
 	});
 	$(document).mouseup( function()
@@ -41,28 +47,47 @@ $(document).ready(function () {
 		}
 	});
 
-
-	player = new fabric.Rect(
+	function startGame()
+	{
+		if ( !interval_1 && gamestate == 0 )
 		{
-			left: canvas.getWidth()/2,
-			top: canvas.getHeight() - 40 - 10,
-			fill: 'green',
-			width: 10,
-			height: 40
+			//prepareResourses();
+			interval_1 = setInterval( processTick, 1000 / 60 );
 		}
-	);
-	canvas.add( player );
+	}
+	function prepareResourses()
+	{
+		player = new fabric.Rect(
+			{
+				left: canvas.getWidth()/2,
+				top: canvas.getHeight() - 40 - 10,
+				fill: 'green',
+				width: 10,
+				height: 40
+			}
+		);
+		canvas.add( player );
 
-	interval_1 = setInterval( processTick, 1000 / 60 );
+		startButton = new fabric.Text(
+			'Press screen to start',
+			{
+				left: canvas.getWidth()/2,
+				top: canvas.getHeight()/2
+			});
+		canvas.add( startButton );
+	}
+
 
 	function gameOver()
 	{
 		var text = new fabric.Text('Game Over\nGained score: ' + score, { left: canvas.getWidth()/2, top: canvas.getHeight()/2});
 		canvas.add(text);
 		clearInterval( interval_1 );
+		gamestate = 99;
 	}
 	function processTick()
 	{
+		canvas.remove( startButton );
 		//console.log( keystate );
 		if ( player )
 		{
@@ -71,9 +96,9 @@ $(document).ready(function () {
 			var y = player.get('top');
 			var new_y;
 
-			if ( keystate[2] == true )
+//			if ( keystate[2] == true )
 			{
-				if ( y < 200 )
+				if ( y < 300 )
 				{
 					speed +=  ( points_per_frame / movements[0] );
 				}
@@ -83,29 +108,39 @@ $(document).ready(function () {
 				}
 				new_y = y - speed;
 			}
-			else
-			{
-				new_y = y;
-			}
+//			else
+//			{
+//				new_y = y;
+//			}
 
 			if ( keystate[0] == true && keystate[1] == false )
 			{
-				new_x =  x - 2;
+				new_x =  x - points_per_frame/2;
 			}
 			else if ( keystate[1] == true && keystate[0] == false )
 			{
-				new_x =  x + 2;
+				new_x =  x + points_per_frame/2;
 			}
 			else
 			{
 				new_x = x;
 			}
 
-			if ( y > canvas.getHeight() )
+			var width = player.get('width')/2;
+
+			if ( new_x - width < 0 )
+			{
+				new_x = 0 + width;
+			}
+			else if ( new_x + width > canvas.getWidth() )
+			{
+				new_x = canvas.getWidth() - width;
+			}
+
+			if ( y - player.get('height')/2 > canvas.getHeight() )
 			{
 				gameOver();
 			}
-
 
 			player.set({left: new_x, top: new_y}).setCoords();
 		}
